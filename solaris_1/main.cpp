b6b2f6aa679c174680f401c7a4518a4498900c97
DenisDubinin <dadubinin@gmail.com>
2016-12-09 21:22:39 +0100

    init

--- solaris_1/main.cpp
+++ solaris_1/main.cpp
@@ -1,15 +1,55 @@
-//
-//  main.cpp
-//  solaris_1
-//
-//  Created by apple on 2016-12-09.
-//  Copyright © 2016 apple. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGTH = 600;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGTH, "Learn OPEN GL", nullptr, nullptr);
+    
+    int screenWidth, screenHeight;
+    glfwGetFramebufferSize(window, &screenWidth, &screenHeight);
+    
+    if (nullptr == window) {
+        std::cout <<  "Failad create GLFW" << std::endl;
+        glfwTerminate();
+        
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    
+    glewExperimental = GL_TRUE;
+    
+    if (GLEW_OK != glewInit()) {
+        std::cout << "Filed to init" << std::endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, screenWidth, screenHeight);
+    
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+    }
+    
+    glfwTerminate();
+    
     return 0;
 }
