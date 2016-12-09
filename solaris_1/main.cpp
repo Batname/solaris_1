f2f96a3e19c62e2aee0f4437d7b2133e4dbcddb4
DenisDubinin <dadubinin@gmail.com>
2016-12-09 08:38:15 +0100

    Initial Commit

---
+++ solaris_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  solaris_1
+//
+//  Created by apple on 2016-12-09.
+//  Copyright © 2016 apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
